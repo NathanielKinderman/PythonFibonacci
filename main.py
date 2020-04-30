f80872e3a27ab65dc5513c98ac99a8fdd598b37b
natek <natekinderman16@gmail.com>
2020-04-30 17:34:51 -0500

    Created def fibonicci_seq which will act like the main functionn. Defined local variables num and series. Will finish while loop logic after moresearch

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("HEllo world")
+#Fibonicci number sequence
+#Have the user enter a number
+#generate a fibonicci sequence
+#which the size is equal to that number
+
+def fibonicci_seq(num):
+    """
+    generates the fibonicci sequence 
+    with the size of num
+    """
+
+    assert num > 0
+
+    series = [1]
+
+
+    while len(series):
+        
